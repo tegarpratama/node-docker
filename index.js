bb98dcb1af3fe8858edf3d84dafbd6f70c8df274
Tegar Pratama <tegar.pratama@kanggo.id>
2022-12-12 13:57:40 +0700

    update index.js

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ app.use(express.json())
 
 app.get('/api/v1/', (req, res) => {
   console.log('Test container load balancing')
-  res.send('<h2>Hi There</h2>')
+  res.send('<h2>Hi There!</h2>')
 })
 
 app.use('/api/v1/posts', postRoutes)
